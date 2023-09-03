43121601e093c130982ce2170e5047c461f5e169
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-09-03 22:37:36 +1000

    Added stub code for db functions

---
+++ src/dbFunctions.ts
@@ -0,0 +1,23 @@
+import { AttachmentBuilder, Client, CommandInteraction } from "discord.js";
+import { parseAndCalculateDifference, parseMessage } from "./util";
+
+export const createEvent = async (
+  client: Client<boolean>,
+  interaction: CommandInteraction
+) => {
+  return;
+};
+
+export const removeEvent = async (
+  client: Client<boolean>,
+  interaction: CommandInteraction
+) => {
+  return;
+};
+
+export const getEvents = async (
+  client: Client<boolean>,
+  interaction: CommandInteraction
+) => {
+  return;
+};
