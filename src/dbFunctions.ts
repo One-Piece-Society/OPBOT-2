aa65fac74ed60b050b5813a8d7e3fbebadd6640f
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-09-03 23:40:14 +1000

    Added stub function for getting result via api

--- src/dbFunctions.ts
+++ src/dbFunctions.ts
@@ -1,5 +1,6 @@
 import { AttachmentBuilder, Client, CommandInteraction } from "discord.js";
 import { parseAndCalculateDifference, parseMessage } from "./util";
+import { supabase } from "./supaBaseClient";
 
 export const createEvent = async (
   client: Client<boolean>,
@@ -19,5 +20,11 @@ export const getEvents = async (
   client: Client<boolean>,
   interaction: CommandInteraction
 ) => {
+  // let { data: OPSOC-Website-Events, error } = await supabase.from('OPSOC-Website-Events').select('*')
+
+  const { data, error } = await supabase.from("OPSOC-Website-Events").select();
+  console.log("hi");
+  console.log(data);
+
   return;
 };
