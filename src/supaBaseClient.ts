445929b0c83be4da6dcb662d001166a97dce9540
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-09-06 01:08:30 +1000

    Style

--- src/supaBaseClient.ts
+++ src/supaBaseClient.ts
@@ -2,15 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 // SOME CONSTANTS
 const Project_URL = process.env?.Project_URL;
-const PUBLIC_ANON_KEY = process.env?.PUBLIC_ANON_KEY;
+const SERVICE_KEY = process.env?.SERVICE_KEY;
 
 if (!Project_URL) throw new Error("Missing Project_URL environment variable.");
-if (!PUBLIC_ANON_KEY)
-  throw new Error("Missing PUBLIC_ANON_KEY environment variable.");
+if (!SERVICE_KEY) throw new Error("Missing SERVICE_KEY environment variable.");
 
 // Create a single supabase client for interacting with your database
 const options = {
   auth: { persistSession: false },
 };
 
-export const supabase = createClient(Project_URL, PUBLIC_ANON_KEY, options);
+export const supabase = createClient(Project_URL, SERVICE_KEY, options);
