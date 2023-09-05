71e3cb5f23b5f165204ac289dbed467f48212f09
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2023-09-06 00:40:27 +1000

    Updated to service role key

--- src/dbFunctions.ts
+++ src/dbFunctions.ts
@@ -6,6 +6,23 @@ export const createEvent = async (
   client: Client<boolean>,
   interaction: CommandInteraction
 ) => {
+  // Validate data
+
+  // Attempt insertion of data
+  const { data, error } = await supabase
+    .from("OPSOC-Website-Events")
+    .insert({
+      title: "testevent",
+      description: "test event description",
+      // startTime: ,
+      // endTime: ,
+      // createdAt: ,
+      // updatedAt: ,
+    })
+    .select();
+  console.log(data);
+  console.log(error);
+
   return interaction.reply("createEvent - Not yet implemented");
 };
 
@@ -22,9 +39,12 @@ export const getEvents = async (
 ) => {
   // let { data: OPSOC-Website-Events, error } = await supabase.from('OPSOC-Website-Events').select('*')
 
-  const { data, error } = await supabase.from("OPSOC-Website-Events").select();
+  const { data, error } = await supabase
+    .from("OPSOC-Website-Events")
+    .select("*");
   console.log("hi");
   console.log(data);
+  console.log(error);
 
   return interaction.reply("getEvents - Not yet implemented");
 };
